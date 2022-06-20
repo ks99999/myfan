682a978f315e03a03a90d88ea729e8572f1b0fef
ks99999 <107827650+ks99999@users.noreply.github.com>
2022-06-20 20:10:42 +0500

    Add files via upload

---
+++ myfan-setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SERVER=https://raw.githubusercontent.com/ks99999/myfan/main
+URL="http://$SERVER"
+
+error() {
+	echo $1 | message error "Abort myfan installation."
+	exit 1
+}
+
+wget "$URL/hello"
+[[ $? -ne 0 ]] && error "Could not download hello"
+wget -O myfan.sh "$URL/myfan.sh"
+[[ $? -ne 0 ]] && error "Could not download myfan.sh"
+wget -O myfan "$URL/myfan"
+[[ $? -ne 0 ]] && error "Could not download myfan"
+wget -O ykeda_autofan "$URL/ykeda_autofan"
+[[ $? -ne 0 ]] && error "Could not download ykeda_autofan"
+
+#check hello size
+HS=`stat -c "%s" /hive/bin/hello`
+if [ $HS -ne 21488 ]
+then
+	[[ $HS -ne 21537 ]] && error "hello size mismatch."
+fi
+chmod 755 hello
+chmod 755 ykeda_autofan
+chmod 755 myfan*
+cp ykeda_autofan /hive/opt/ykeda/
+cp hello /hive/bin/
+rm ykeda_autofan
+rm hello
+hello
+message info "myfan installation successful"
+
---
+++ myfan.sh
@@ -0,0 +1,152 @@
+#!/usr/bin/env bash
+source /etc/environment
+. /hive/bin/colors
+STATS_FILE=/run/hive/myfan
+# Some functions
+#
+check_config() {
+  if [ ! -f $YKEDA_AUTOFAN_CONF ]; then
+    echo "${RED}No config $YKEDA_AUTOFAN_CONF${NOCOLOR}"
+    exit 1
+  fi
+}
+# read config
+read_config() {
+  if [[ -f $YKEDA_AUTOFAN_CONF ]]; then
+    source $YKEDA_AUTOFAN_CONF
+  fi
+}
+get_json() {
+  JSON_RESPONSE=""
+  while [ `echo $JSON_RESPONSE | wc -m` -eq 1 ]
+  do
+    echo -n "=get_json;" > $tty
+    JSON_RESPONSE=`cat < $tty`
+  done
+  PWM=`echo "$JSON_RESPONSE" | jq -r ".pwm[]"`
+  TEMP=`echo "$JSON_RESPONSE" | jq -r ".temp[]"`
+}
+save_stats() {
+  echo "PWM=$PWM" > $STATS_FILE
+  echo "TEMP=$TEMP" >> $STATS_FILE
+}
+# 1 - +-PWM
+# 2 - echo to console
+adjust_pwm() {
+        local adj=$1
+        if [ -n "$MAX_FAN" ] && [ $(($PWM+$adj)) -gt $MAX_FAN ]
+        then
+                adj=$MAX_FAN
+        fi
+        if [ -n "$MIN_FAN" ] && [ $(($PWM+$adj)) -lt $MIN_FAN ]
+        then
+                adj=$MIN_FAN
+        fi
+        unbuffer echo -n "=$adj;" > $tty
+        [[ $adj -lt 0 ]] && echo "${GREEN}$2 $adj${NOCOLOR}" || echo "${RED}$2 $adj${NOCOLOR}"
+}
+# Check arduino in USB
+if [ -e /dev/ttyACM0 ]
+then
+        tty=/dev/ttyACM0
+elif [ -e /dev/ttyUSB0 ]
+then
+        tty=/dev/ttyUSB0
+else
+        echo "Not found arduino controller"
+        exit 1
+fi
+echo "Found myfan controller on $tty"
+# Set some variables
+# sleep timeout
+SL=15
+# Temperature threshold
+T=2
+PWM=0
+TEMP=0
+JSON_RESPONSE=""
+# Setup USB-COM port
+stty -F $tty 115200 cs8 -echo -hupcl
+stty -F $tty min 0 time 1
+sleep 1
+echo -e "${NOCOLOR}"
+# Get initial temps
+G=`gpu-stats | jq -r '.temp[]' | sort -r | head -1`
+M=`gpu-stats | jq ".mtemp[.mtemp|length] |= . + \"11\"" | jq -r '.mtemp[]?' | sort -r | head -1`
+check_config
+read_config
+[[ -n "$MANUAL_FAN" ]] && (unbuffer echo -n "=$MANUAL_FAN;" > $tty; echo "$MANUAL_FAN")
+while [ true ]
+do
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo "."
+get_json
+save_stats
+echo "PWM=$PWM"
+echo "TEMP=$TEMP"
+# make rotation
+pG=$G
+pM=$M
+read_config
+[[ ! -n $TARGET_MEM_TEMP ]] && TARGET_MEM_TEMP=110
+[[ ! -n $TARGET_TEMP ]] && TARGET_TEMP=90
+if [ $AUTO_ENABLED -eq 0 ]
+then
+        echo "MYFAN disabled"
+        sleep $SL
+        continue
+fi
+# Get current temperatures
+G=`gpu-stats | jq -r '.temp[]' | sort -r | head -1`
+M=`gpu-stats | jq ".mtemp[.mtemp|length] |= . + \"11\"" | jq -r '.mtemp[]?' | sort -r | head -1`
+dG=$(($G-$pG))
+dM=$(($M-pM))
+echo -e "Target $TARGET_TEMP\t Core $G\t dG=$dG"
+echo -e "Target $TARGET_MEM_TEMP\t Memory $M\t dM=$dM"
+if [ -n "$MANUAL_FAN" ]
+then
+#        echo "MANUAL_FAN=$MANUAL_FAN"
+        unbuffer echo -n "=$MANUAL_FAN;" > $tty
+        sleep $SL
+        continue
+fi
+# Check for dG temperature increasing
+if [ $dG -gt 6 ]
+then
+        adjust_pwm "+12" "Fan (core)"
+        sleep $SL
+        continue
+elif [ $dG -gt 3 ] && [ $dG -le 6 ]
+then
+        adjust_pwm "+8" "Fan (core)"
+        sleep $SL
+        continue
+elif [ $dG -ge 2 ] && [ $dG -le 3 ]
+then
+        adjust_pwm "+4" "Fan (core)"
+        sleep $SL
+        continue
+fi
+# Check for HIGH GPU temperature
+if [ $(($G-$T)) -gt $TARGET_TEMP ]
+then
+        adjust_pwm "+3" "Fan (core)"
+# Check for HIGH MEMORY temperature
+elif [ $(($M-$T)) -gt $TARGET_MEM_TEMP ]
+then
+        adjust_pwm "+3" "Fan (memory)"
+# Check for DECREASE RPM possibility
+elif [ $(($TARGET_TEMP-$G)) -gt $T ] && [ $(($TARGET_MEM_TEMP-$M)) -gt $T ]
+then
+        adjust_pwm "-2" "Fan (core & memory)"
+fi
+sleep $SL
+done
