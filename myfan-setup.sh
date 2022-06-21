1e1bc432f3ceb362050ce144d9d60c3c2134c04c
ks99999 <107827650+ks99999@users.noreply.github.com>
2022-06-21 20:49:00 +0500

    Update myfan-setup.sh

--- myfan-setup.sh
+++ myfan-setup.sh
@@ -3,11 +3,11 @@
 URL=https://raw.githubusercontent.com/ks99999/myfan/main
 
 error() {
-	echo $1 | message error "Abort myfan installation."
-	exit 1
+        echo $1 | message error "Abort myfan installation."
+        exit 1
 }
 
-wget "$URL/hello"
+wget -O hello "$URL/hello"
 [[ $? -ne 0 ]] && error "Could not download hello"
 wget -O myfan.sh "$URL/myfan.sh"
 [[ $? -ne 0 ]] && error "Could not download myfan.sh"
@@ -18,10 +18,12 @@ wget -O ykeda_autofan "$URL/ykeda_autofan"
 
 #check hello size
 HS=`stat -c "%s" /hive/bin/hello`
-if [ $HS -ne 21508 ]
+if [ $HS -eq 21508 ]
 then
-	[[ $HS -ne 21537 ]] && error "hello size mismatch."
+        echo "hello size matched to modified" | message info "myfan already installed." payload > /dev/null
+        exit 1
 fi
+[[ $HS -ne 21537 ]] && error "hello size mismatch."
 chmod 755 hello
 chmod 755 ykeda_autofan
 chmod 755 myfan*
@@ -31,4 +33,5 @@ rm ykeda_autofan
 rm hello
 hello
 message info "myfan installation successful"
+
 
