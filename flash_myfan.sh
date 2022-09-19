a6220ab7497613c3ba6bf823896b670d483c5580
ks99999 <107827650+ks99999@users.noreply.github.com>
2022-09-19 10:27:53 +0500

    Update flash_myfan.sh

--- flash_myfan.sh
+++ flash_myfan.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-screen -r myfan -X quit
-sleep 1
+
+stop_myfan(){
+  local screens=(`screen -ls myfan | grep -Po "\K[0-9]+(?=\.myfan)" | sort --unique`)
+  for pid in "${screens[@]}"; do
+    timeout 1 screen -S $pid.myfan -X quit
+  done
+}
+
+stop_myfan
 /usr/bin/avrdude -p m328p -c arduino -P /dev/ttyACM0 -b 57600 -U flash:w:myfan3.ino.hex:i
 hello
